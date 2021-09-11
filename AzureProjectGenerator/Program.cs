f14b515ef1912cefa515d7fa14bf83979b5f97c9
Lucas Hipólito <lucashi@algartech.com>
2021-09-11 12:10:28 -0300

    Add project files.

---
+++ AzureProjectGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AzureProjectGenerator
+{
+    internal class Program
+    {
+        private class OsConfig
+        {
+            internal bool isWindows;
+            internal bool isMac;
+            internal bool isLinux;
+            public OsConfig()
+            {
+                isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+                isMac = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+                isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+
+            var osConfig = new OsConfig();
+            Console.WriteLine("Current OS: " + RuntimeInformation.OSDescription);
+            Console.WriteLine($"Is windows: {osConfig.isWindows}");
+            Console.WriteLine($"Is linux: {osConfig.isLinux}");
+            Console.WriteLine($"Is mac: {osConfig.isMac}");
+        }
+
+        private void GeneratePack(OsConfig os)
+        {
+            /*
+             * on the template folder
+             * 
+             * WINDOWS:
+             * dotnet new -i .\
+             * 
+             * 
+             * LINUX:
+             * dotnet new -i ./
+             */
+        }
+    }
+}
---
+++ AzureProjectGenerator/working/templates/extensions/CommonExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AzureProjectGenerator.working.templates.extensions
+{
+    public static class StringExtensions
+    {
+        public static string Reverse(this string value)
+        {
+            var tempArray = value.ToCharArray();
+            Array.Reverse(tempArray);
+            return new string(tempArray);
+        }
+    }
