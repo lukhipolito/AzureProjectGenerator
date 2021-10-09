535c58d19df9860f75d36c3a283f7239cba2d490
Lucas Hipólito <lucashi@algartech.com>
2021-10-09 19:48:42 -0300

    fix: bash script

--- AzureProjectGenerator/Program.cs
+++ AzureProjectGenerator/Program.cs
@@ -119,25 +119,25 @@ namespace AzureProjectGenerator
             }
             else
             {
-                string script = $@"cd {templatesPath}
-                        dotnet pack
-                        dotnet new -i ./bin/Debug/AzureProjectTemplate.1.0.0.nupkg
-                        cd {output}
-                        mkdir {name}.API
-                        cd {name}.API
-                        dotnet new azureprojectapi -n {name}
-                        cd ..
-                        mkdir {name}.Domain
-                        cd {name}.Domain
-                        dotnet new azureprojectdomain -n {name}
-                        cd ..
-                        mkdir {name}.Infra
-                        cd {name}.Infra
-                        dotnet new azureprojectinfra -n {name}
-                        cd ..
-                        dotnet new sln -n {name}
-                        dotnet sln add {name}.API/{name}.API.csproj
-                        dotnet sln add {name}.Domain/{name}.Domain.csproj
+                string script = $@"cd {templatesPath}                                \
+                        dotnet pack                                                  \
+                        dotnet new -i ./bin/Debug/AzureProjectTemplate.1.0.0.nupkg   \
+                        cd {output}                                                  \
+                        mkdir {name}.API                                             \
+                        cd {name}.API                                                \
+                        dotnet new azureprojectapi -n {name}                         \
+                        cd ..                                                        \
+                        mkdir {name}.Domain                                          \
+                        cd {name}.Domain                                             \
+                        dotnet new azureprojectdomain -n {name}                      \
+                        cd ..                                                        \
+                        mkdir {name}.Infra                                           \
+                        cd {name}.Infra                                              \
+                        dotnet new azureprojectinfra -n {name}                       \
+                        cd ..                                                        \
+                        dotnet new sln -n {name}                                     \
+                        dotnet sln add {name}.API/{name}.API.csproj                  \
+                        dotnet sln add {name}.Domain/{name}.Domain.csproj            \
                         dotnet sln add {name}.Infra/{name}.Infra.csproj";
 
                 _ = script.Bash(os.logger).Result;
