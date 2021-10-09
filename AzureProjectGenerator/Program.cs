928173aa6d1f72265168b0b5cb8c93a903176605
Lucas Hipólito <lucashi@algartech.com>
2021-10-09 20:00:02 -0300

    chore: testing indirect bash script call

--- AzureProjectGenerator/Program.cs
+++ AzureProjectGenerator/Program.cs
@@ -119,26 +119,7 @@ namespace AzureProjectGenerator
             }
             else
             {
-                string script = $@"cd {templatesPath}                                \
-                        dotnet pack                                                  \
-                        dotnet new -i ./bin/Debug/AzureProjectTemplate.1.0.0.nupkg   \
-                        cd {output}                                                  \
-                        mkdir {name}.API                                             \
-                        cd {name}.API                                                \
-                        dotnet new azureprojectapi -n {name}                         \
-                        cd ..                                                        \
-                        mkdir {name}.Domain                                          \
-                        cd {name}.Domain                                             \
-                        dotnet new azureprojectdomain -n {name}                      \
-                        cd ..                                                        \
-                        mkdir {name}.Infra                                           \
-                        cd {name}.Infra                                              \
-                        dotnet new azureprojectinfra -n {name}                       \
-                        cd ..                                                        \
-                        dotnet new sln -n {name}                                     \
-                        dotnet sln add {name}.API/{name}.API.csproj                  \
-                        dotnet sln add {name}.Domain/{name}.Domain.csproj            \
-                        dotnet sln add {name}.Infra/{name}.Infra.csproj";
+                string script = $"scripts/generate.sh -- param name={name} output={output} templatesPath={templatesPath}";
 
                 script.Bash(os.logger).Wait();
             } 
